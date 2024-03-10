fa4d45304faa28848c49b27dc2e8923eeef66042
Tom Childress <childressiv.dev@gmail.com>
2024-03-10 15:06:14 -0700

    Add import Image to /app/page.tsx

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -3,6 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts.ts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
